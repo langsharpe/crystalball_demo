4262e88d2267d64eda3792e4d14d92bf7b879aa7
Lang Sharpe <lang.sharpe@fivegoodfriends.com.au>
2021-03-06 11:26:18 +1000

    Add Post/Comments relationship and Post tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#title" do
+    it "should accept a string" do
+      post = Post.create!
+      post.title = "Hello"
+      expect(post.title).to eq "Hello"
+    end
+  end
+
+  describe "#comments" do
+    it "should allow comments" do
+      post = Post.create!
+      post.comments.create!
+      expect(post.comments.length).to eq 1
+    end
+  end
 end
